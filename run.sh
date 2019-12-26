f70806c780930e302c71f15d67a6d6f0d7af4911
Paul Crompton <beethoven89.paul@gmail.com>
2019-12-26 11:03:57 -0500

    add ability to add custom greeting

--- run.sh
+++ run.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo Hello World!
+greeting='World'
+
+while [ $# -gt 1 ]; do
+  case "$1" in
+    -g|--greeting)
+      shift
+      greeting=$1
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+echo "Hello $greeting!"
