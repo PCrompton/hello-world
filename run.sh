fd1e5793095579cdc8e83a4b6a92bdc04f174af4
Paul Crompton <beethoven89.paul@gmail.com>
2019-12-26 12:57:00 -0500

    Add help option and more error handling

--- run.sh
+++ run.sh
@@ -2,10 +2,18 @@
 
 addressee='World'
 
-while [ $# -gt 1 ]; do
+while [ $# -gt 0 ]; do
   case "$1" in
+    -h|--help)
+      echo 'Options: [-a|--addressee <text>]'
+      exit 1
+      ;;
     -a|--addressee)
       shift
+      if [ -z $1 ]; then
+        echo "No addressee provided"
+        exit 1
+      fi
       addressee=$1
       shift
       ;;
